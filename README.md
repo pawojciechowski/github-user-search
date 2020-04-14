d2b6df38993ca2bfafa977872acd50aa770c5c97
Piotr Wojciechowski <wojciechowski.piotr.adam@gmail.com>
2020-04-14 14:15:03 +0200

    Update README.md

--- README.md
+++ README.md
@@ -26,3 +26,7 @@ You can try it [here](https://githubusersearch.pawojciechowski.ovh/).
 - [x] HMR for both components and state
 - [x] Add CI & CD and deploy it to the server
 - [x] Add skeletons
+
+## Notes
+
+- To make this app more resilent to errors there can be [Error Boundaries](https://reactjs.org/docs/error-boundaries.html) added, but no time for that unfortunately.
--- src/modules/themes/components/ThemeProvider.tsx
+++ src/modules/themes/components/ThemeProvider.tsx
@@ -1,15 +1,11 @@
 import React, { PropsWithChildren } from 'react';
 import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
-import { ThemeName } from '../types';
 import { themes } from '../themes';
 import GlobalStyles from './GlobalStyles';
 
 function ThemeProvider({ children }: PropsWithChildren<{}>) {
-  // TODO: Get theme from state
-  const themeName: ThemeName = 'default';
-
   return (
-    <StyledComponentsThemeProvider theme={themes[themeName]}>
+    <StyledComponentsThemeProvider theme={themes['default']}>
       <GlobalStyles />
       {children}
     </StyledComponentsThemeProvider>
